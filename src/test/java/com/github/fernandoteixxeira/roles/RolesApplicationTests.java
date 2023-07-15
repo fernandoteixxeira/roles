05056c3b69a010cc9fc8695d902d096d548889f3
Fernando Teixeira Alves de Araujo <fernandoftaa@yahoo.com.br>
2023-07-15 16:53:33 -0300

    feat: Creation of project structure

---
+++ src/test/java/com/github/fernandoteixxeira/roles/RolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fernandoteixxeira.roles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
